1abde40063dd13527c5e84ed3b9ee5d76783108a
Deepanshu sharma <U15CO061@coed.svnit.ac.in>
2017-03-18 01:33:42 +0530

    Add api.ai api

--- app.py
+++ app.py
@@ -76,11 +76,18 @@ def process_text_message(msg):
     ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
     request = ai.text_request()
     request.lang = 'en'  # optional, default value equal 'en'
-    request.session_id = "Ajf54Trh"
+    request.session_id = "Ajf54Trh" #generate random session_id later
     request.query = msg
-    response = request.getresponse()
-    log(response.read())
-    return "hello got it"
+
+    response = json.loads(request.getresponse().read().decode('utf-8'))
+    log(response)
+    responseStatus = response['status']['code']
+    if (responseStatus == 200):
+        # Sending the textual response of the bot.
+        return (response['result']['fulfillment']['speech'])
+
+    else:
+        return ("Sorry, I couldn't understand that question")
 
 def send_message(recipient_id, message_text):
 
