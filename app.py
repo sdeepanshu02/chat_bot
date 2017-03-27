c53bd63a838dd90871559de29bf3de95390bb0ea
Tanishka1997 <u15co059@coed.svnit.ac.in>
2017-03-27 23:48:37 +0530

    bug fixing

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from flask import Flask, request, make_response, render_template
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime,timedelta,date
 import wikipedia
-from operator import itemgetter
+from operator import itemgetter,attrgetter
 
 app = Flask(__name__)
 CLIENT_ACCESS_TOKEN = '6dc4dd64472140deaad4cbe8f39ff10f'   #apiai client access_token
@@ -171,7 +171,7 @@ def getdata():
             place_rating = place['rating']
             det_of_place={'rating':place_rating,'name_of_place':name_of_place,'address':address}
             list_of_places.append(det_of_place)
-        sorted(list_of_places,key=lambda places:places['rating'])
+        sorted(list_of_places,key=attrgetter('rating'),reverse=True)
         result=""
         r=""
         for place in list_of_places[0:4]:
